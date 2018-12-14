f7bcd8b6bf6941d4dc0f4f90f804377e9f823b03
DESKTOP-NJK7UBI\别动我名字 <172212595@qq.com>
2018-12-14 23:06:35 +0800

    tearDown() 方法中注释截图

--- util/test_star_end.py
+++ util/test_star_end.py
@@ -10,11 +10,11 @@ class TestStarEnd(unittest.TestCase):
 
     def setUp(self):
         # 在是python3.x 中，如果在这里初始化driver ，因为3.x版本 unittest 运行机制不同，会导致用力失败时截图失败
-        self.driver = CreateDriver()
         self.imgs = []
+        self.driver = CreateDriver(self.imgs)
         self.logs = log.log_message('初始化')
         self.logs.logger.info('启动')
 
     def tearDown(self):
-        self.imgs.append(self.driver.add_img())
+        # self.imgs.append(self.driver.add_img())
         self.driver.quit()
