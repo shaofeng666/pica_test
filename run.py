89f18b76d26c23c3de0eaed2e2f221e791dd1b7b
DESKTOP-NJK7UBI\别动我名字 <172212595@qq.com>
2018-12-17 23:20:49 +0800

    尝试添加多个路径下的case

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# @Date    : 2017-08-11 15:34:37
-# @Author  : lileilei 
+# @Date    : 2018-08-11 15:34:37
+# @Author  : Victor
 from suite.testsuite import create_report
 from  suite import  testsuite
 from suite.send_email import send_email
 
 if __name__ == '__main__':
-	# testsuite.run_case()
+	# testsuite.run_case()# 调试、不生成报告
+	# testsuite.run_this_case('n',pitent)#调试 运行指定.py文件 生成报告
 	testsuite.create_report('n')# 生成报告
 	# send_email()# 发送邮件
+
+
 
